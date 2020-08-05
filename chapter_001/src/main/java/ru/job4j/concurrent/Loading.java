6dcdfde255bf5a9b3c8703fcf6f78ca2fba6f5a2
alexanderlebedev1989 <AlexanderLebedev1989@gmail.com>
2020-08-05 18:45:20 +0500

    3.1. Прерывание блокированной нити. [#336770]

--- chapter_001/src/main/java/ru/job4j/concurrent/Loading.java
+++ chapter_001/src/main/java/ru/job4j/concurrent/Loading.java
@@ -1,22 +1,26 @@
 package ru.job4j.concurrent;
 
 public class Loading {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread thread = new Thread(
                 () -> {
-                    try {
-                        System.out.println("Start loading ... ");
-                        for (int i = 0; i < 100; i++) {
-                            System.out.print("\rLoading : " + i  + "%");
-                            Thread.sleep(1000);
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println("Start loading ... ");
+                            for (int i = 0; i < 100; i++) {
+                                System.out.print("\rLoading : " + i + "%");
+                                Thread.sleep(100);
+                            }
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
                         }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
                     }
                 }
         );
         thread.start();
         System.out.println("Main");
+        Thread.sleep(1000);
+        thread.interrupt();
     }
 }
 
