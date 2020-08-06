fb2e5c655cdebf5a8fa0f7a2382874bff6ee8d60
alexanderlebedev1989 <AlexanderLebedev1989@gmail.com>
2020-08-06 12:17:34 +0500

    update

---
+++ chapter_001/src/main/java/ru/job4j/concurrent/Test.java
@@ -0,0 +1,9 @@
+package ru.job4j.concurrent;
+
+public class Test {
+    public static void main(String[] args) {
+       long start = System.currentTimeMillis();
+       long end = 1;
+        System.out.println(start + 1);
+    }
+}
