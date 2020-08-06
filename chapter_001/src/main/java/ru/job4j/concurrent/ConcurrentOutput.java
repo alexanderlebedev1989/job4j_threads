e1017cf3708825924eff25021200e6cb07876458
alexanderlebedev1989 <AlexanderLebedev1989@gmail.com>
2020-08-06 12:40:04 +0500

    update

--- chapter_001/src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
+++ chapter_001/src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -2,6 +2,7 @@ package ru.job4j.concurrent;
 
 public class ConcurrentOutput {
     public static void main(String[] args) {
+
         Thread another = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
