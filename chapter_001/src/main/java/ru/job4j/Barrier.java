6e520c58cabed6ba5d399b7e7c0d850a04b05b18
alexanderlebedev1989 <AlexanderLebedev1989@gmail.com>
2020-08-16 12:02:47 +0500

    3.4. Временное хранение. git stash. [#336627]

--- chapter_001/src/main/java/ru/job4j/Barrier.java
+++ chapter_001/src/main/java/ru/job4j/Barrier.java
@@ -1,8 +1,8 @@
 package ru.job4j;
 
 public class Barrier {
-    private boolean flag = false;
 
+    private boolean flag = false;
     private final Object monitor = this;
 
     public void on() {
